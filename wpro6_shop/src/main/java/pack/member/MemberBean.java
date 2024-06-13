6f58701ee8499d3c61fd1ac103f4238e840f79b6
yeobin <duqqnls123@naver.com>
2024-06-13 17:35:38 +0900

    [SHOPPING SITE 3 - 로그인 + 멤버]

---
+++ wpro6_shop/src/main/java/pack/member/MemberBean.java
@@ -0,0 +1,14 @@
+package pack.member;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+
+public class MemberBean {
+	private String id, passwd, name, email, phone,zipcode,address,job;
+	
+}
---
+++ wpro6_shop/src/main/java/pack/member/ZipcodeDto.java
@@ -0,0 +1,12 @@
+package pack.member;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class ZipcodeDto {
+	private String zipcode, area1, area2, area3, area4;
+}
