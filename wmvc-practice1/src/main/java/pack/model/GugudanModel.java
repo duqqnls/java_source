bd169f86e04e224ad68361c9b7f069aff1411797
yeobin <duqqnls123@naver.com>
2024-07-01 17:09:04 +0900

    [MVC 1]

---
+++ wmvc-practice1/src/main/java/pack/model/GugudanModel.java
@@ -0,0 +1,11 @@
+package pack.model;
+
+import java.util.ArrayList;
+
+public class GugudanModel {
+	public ArrayList<String> getGugudan(String hobby){
+		ArrayList<String> list = new ArrayList();
+		
+		return list;
+	}
+}
